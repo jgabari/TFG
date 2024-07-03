89d4dcc6baecb6b27df641b94b9572fb215eeb61
jgabari <javigabari@gmail.com>
2024-07-03 17:39:59 +0200

    correcciones standardjs

--- v2/game2.js
+++ v2/game2.js
@@ -3,263 +3,259 @@
  * over time in a software system.
  */
 class SimSys {
-    // Operational ranges of the system.
-    MAX_HEALTH = 100;
-    MIN_HEALTH = 0;
-    MIN_BUGS = 0;
+  // Operational ranges of the system.
+  MAX_HEALTH = 100
+  MIN_HEALTH = 0
+  MIN_BUGS = 0
 
-
-    /**
+  /**
      * Constructor with default values.
      */
-    constructor() {
-        // Default configuration for the simulation.
-        this.config = {
-            init: {
-                budget: 100,
-                features: 0,
-                bugs: 0,
-                health: 100,
-            },
-            factors: {
-                l: 1, // Lehman's Law: inherent degradation
-                re: 1, // Refactoring effectiveness
-                fae: 1, // Feature addition effectiveness
-                bfe: 1, // Bug-fix effectiveness
-                nbp: 1, // New bug proneness
-                vf: 1, // Value generated per feature
-                vb: 1, // Value lost per bug
-                bm: 1, // Budget multiplier
-            },
-        };
-        this.resetSimulation();
+  constructor () {
+    // Default configuration for the simulation.
+    this.config = {
+      init: {
+        budget: 100,
+        features: 0,
+        bugs: 0,
+        health: 100
+      },
+      factors: {
+        l: 1, // Lehman's Law: inherent degradation
+        re: 1, // Refactoring effectiveness
+        fae: 1, // Feature addition effectiveness
+        bfe: 1, // Bug-fix effectiveness
+        nbp: 1, // New bug proneness
+        vf: 1, // Value generated per feature
+        vb: 1, // Value lost per bug
+        bm: 1 // Budget multiplier
+      }
     }
+    this.resetSimulation()
+  }
 
-    /**
+  /**
      * Deserialize a saved simulation state from a browser cookie.
      * @param {string} serialized Serialized simulation state.
      * @returns {SimSys} A new instance of SimSys populated with the deserialized data.
      */
-    static fromCookie(serialized) {
-        const sim = new SimSys();
-        const data = JSON.parse(serialized);
-        sim.iteration = data.iteration;
-        sim.config = data.config;
-        sim.stats = data.stats;
-        sim.budget = data.budget;
-        return sim;
-    }
+  static fromCookie (serialized) {
+    const sim = new SimSys()
+    const data = JSON.parse(serialized)
+    sim.iteration = data.iteration
+    sim.config = data.config
+    sim.stats = data.stats
+    sim.budget = data.budget
+    return sim
+  }
 
-    resetSimulation() {
-        // Current iteration of the simulation
-        this.iteration = 0;
-        // Current stats for the simulation.
-        this.stats = {
-            features: this.config.init.features,
-            bugs: this.config.init.bugs,
-            health: this.config.init.health,
-            value:
+  resetSimulation () {
+    // Current iteration of the simulation
+    this.iteration = 0
+    // Current stats for the simulation.
+    this.stats = {
+      features: this.config.init.features,
+      bugs: this.config.init.bugs,
+      health: this.config.init.health,
+      value:
                 this.config.init.features * this.config.factors.vf -
-                this.config.init.bugs * this.config.factors.vb,
-        };
-        // Budget allocation for the current iteration.
-        this.budget = {
-            available: this.config.init.budget,
-            feature: 0,
-            bugfix: 0,
-            refactor: 0,
-        };
-
-        // Show the values in screen
-        this.updateDisplays();
+                this.config.init.bugs * this.config.factors.vb
+    }
+    // Budget allocation for the current iteration.
+    this.budget = {
+      available: this.config.init.budget,
+      feature: 0,
+      bugfix: 0,
+      refactor: 0
     }
 
-    /**
+    // Show the values in screen
+    this.updateDisplays()
+  }
+
+  /**
      * Executes a single iteration of the simulation.
      * @param {number} round Precision for rounding numbers.
      * @throws {Error} When the budget allocation is invalid or system health drops to zero.
      */
-    runIteration(round) {
-        const f = this.config.factors; // Factors
-        const b = this.budget; // Current budget
-        const s = this.stats; // Current stats
+  runIteration (round) {
+    const f = this.config.factors // Factors
+    const b = this.budget // Current budget
+    const s = this.stats // Current stats
 
-        // if (b.available !== b.feature + b.bugfix + b.refactor) {
-        //     throw new Error("Budget is not valid.");
-        // }
+    // if (b.available !== b.feature + b.bugfix + b.refactor) {
+    //     throw new Error("Budget is not valid.");
+    // }
 
-        this.iteration++;
+    this.iteration++
 
-        // Update health status
-        s.health = s.health + b.refactor * f.re - f.l;
-        s.health = Math.min(this.MAX_HEALTH, s.health);
-        s.health = Math.max(this.MIN_HEALTH, s.health);
-        s.health = this.round(s.health, round);
+    // Update health status
+    s.health = s.health + b.refactor * f.re - f.l
+    s.health = Math.min(this.MAX_HEALTH, s.health)
+    s.health = Math.max(this.MIN_HEALTH, s.health)
+    s.health = this.round(s.health, round)
 
-        if (s.health === 0) {
-            throw new Error("Your system became unmaintainable and collapsed.");
-        }
-
-        // Update features status
-        s.features = s.features + b.feature * s.health * f.fae;
-        s.features = this.round(s.features, round);
-
-        // Update bug status
-        const fixedbugs = b.bugfix * s.health * f.bfe;
-        const newbugs = (b.feature / s.health) * f.nbp;
-        s.bugs = s.bugs - fixedbugs + newbugs;
-        s.bugs = Math.max(this.MIN_BUGS, s.bugs);
-        s.bugs = this.round(s.bugs, round);
-
-        // Calculate system value
-        const featuremerit = s.features * f.vf;
-        const bugthreat = s.bugs * f.vb;
-        s.value = featuremerit - bugthreat;
-        s.value = this.round(s.value, round);
-
-        // Calculate available budget for the next iteration
-        b.available = s.value * f.bm;
-        b.available = this.round(b.available, round);
-
-        // Show the values in screen
-        this.updateDisplays();
-        budget_display.innerHTML = b.available;
+    if (s.health === 0) {
+      throw new Error('Your system became unmaintainable and collapsed.')
     }
 
-    /**
+    // Update features status
+    s.features = s.features + b.feature * s.health * f.fae
+    s.features = this.round(s.features, round)
+
+    // Update bug status
+    const fixedbugs = b.bugfix * s.health * f.bfe
+    const newbugs = (b.feature / s.health) * f.nbp
+    s.bugs = s.bugs - fixedbugs + newbugs
+    s.bugs = Math.max(this.MIN_BUGS, s.bugs)
+    s.bugs = this.round(s.bugs, round)
+
+    // Calculate system value
+    const featuremerit = s.features * f.vf
+    const bugthreat = s.bugs * f.vb
+    s.value = featuremerit - bugthreat
+    s.value = this.round(s.value, round)
+
+    // Calculate available budget for the next iteration
+    b.available = s.value * f.bm
+    b.available = this.round(b.available, round)
+
+    // Show the values in screen
+    this.updateDisplays()
+    budgetDisp.innerHTML = b.available
+  }
+
+  /**
      * Round a number to a given precision.
      * @param {number} number Number to be rounded.
      * @param {number} precision Number of decimal places.
      * @returns {number} Rounded number.
      */
-    round(number, precision) {
-        const factor = Math.pow(10, precision);
-        return Math.round(number * factor) / factor;
-    }
+  round (number, precision) {
+    const factor = Math.pow(10, precision)
+    return Math.round(number * factor) / factor
+  }
 
-    /**
+  /**
      * Serialize the current simulation state for saving as a browser cookie.
      * @returns {string} Serialized simulation state.
      */
-    toString() {
-        return JSON.stringify({
-            iteration: this.iteration,
-            stats: this.stats,
-            budget: this.budget,
-            config: this.config,
-        });
-    }
+  toString () {
+    return JSON.stringify({
+      iteration: this.iteration,
+      stats: this.stats,
+      budget: this.budget,
+      config: this.config
+    })
+  }
 
-
-    updateSum(ev) {
-        let total = sliders.reduce((sum, slider) => sum + parseInt(slider.value), 0);
-        if (total > 100) {
-            let diff = total - 100;
-            for (let i = prevInputs.length - 1; i >= 0 && diff > 0; i--) {
-                if (prevInputs[i] === ev.target) {
-                    i -= 1;
-                }
-                let value = parseInt(prevInputs[i].value);
-                if (value >= diff) {
-                    prevInputs[i].value = value - diff;
-                    diff = 0;
-                } else {
-                    prevInputs[i].value = 0;
-                    diff -= value;
-                }
-            }
+  updateSum (ev) {
+    const total = sliders.reduce((sum, slider) => sum + parseInt(slider.value), 0)
+    if (total > 100) {
+      let diff = total - 100
+      for (let i = prevInputs.length - 1; i >= 0 && diff > 0; i--) {
+        if (prevInputs[i] === ev.target) {
+          i -= 1
         }
-        //percents.forEach((id, i) => document.getElementById(id).textContent = sliders[i].value);
-        values.forEach((id, i) => document.getElementById(id).textContent = sliders[i].value);
-        if (prevInputs[prevInputs.length - 1] !== ev.target) {
-            prevInputs.push(ev.target);
+        const value = parseInt(prevInputs[i].value)
+        if (value >= diff) {
+          prevInputs[i].value = value - diff
+          diff = 0
+        } else {
+          prevInputs[i].value = 0
+          diff -= value
         }
-        while (prevInputs.length > sliders.length) {
-            prevInputs.shift();
-        }
-        mySys.budget.feature = parseInt(sliders[0].value)*mySys.budget.available/100;
-        mySys.budget.bugfix = parseInt(sliders[1].value)*mySys.budget.available/100;
-        mySys.budget.refactor = parseInt(sliders[2].value)*mySys.budget.available/100;
+      }
     }
-
-    updateDisplays() {
-        value_display.innerHTML = this.stats.value;
-        features_display.innerHTML = this.stats.features;
-        bugs_display.innerHTML = this.stats.bugs;
-        health_display.innerHTML = this.stats.health;
-        budget_display.innerHTML = this.budget.available;
+    // percents.forEach((id, i) => document.getElementById(id).textContent = sliders[i].value);
+    values.forEach((id, i) => (document.getElementById(id).textContent = sliders[i].value))
+    if (prevInputs[prevInputs.length - 1] !== ev.target) {
+      prevInputs.push(ev.target)
     }
+    while (prevInputs.length > sliders.length) {
+      prevInputs.shift()
+    }
+    mySys.budget.feature = parseInt(sliders[0].value) * mySys.budget.available / 100
+    mySys.budget.bugfix = parseInt(sliders[1].value) * mySys.budget.available / 100
+    mySys.budget.refactor = parseInt(sliders[2].value) * mySys.budget.available / 100
+  }
+
+  updateDisplays () {
+    valueDisp.innerHTML = this.stats.value
+    featuresDisp.innerHTML = this.stats.features
+    bugsDisp.innerHTML = this.stats.bugs
+    healthDisp.innerHTML = this.stats.health
+    budgetDisp.innerHTML = this.budget.available
+  }
 }
 
 // Get elements from HTML
-const iterate_btn = document.getElementById("iterate_btn");
-const reset_btn = document.getElementById("reset_btn");
-const download_btn = document.getElementById("download_btn");
-const upload_btn = document.getElementById("upload_btn");
-const value_display = document.getElementById("value_display");
-const features_display = document.getElementById("features_display");
-const bugs_display = document.getElementById("bugs_display");
-const health_display = document.getElementById("health_display");
-const budget_display = document.getElementById("budget_display");
-let inputs = ["features_input", "bugs_input", "refactor_input"];
-let sliders = inputs.map(id => document.getElementById(id));
-let values = inputs.map(id => id.replace("input", "value"));
-//let percents = inputs.map(id => id.replace("input", "percent"));
+const iterateBtn = document.getElementById('iterate_btn')
+const resetBtn = document.getElementById('reset_btn')
+const downloadBtn = document.getElementById('download_btn')
+const uploadBtn = document.getElementById('upload_btn')
+const valueDisp = document.getElementById('value_display')
+const featuresDisp = document.getElementById('features_display')
+const bugsDisp = document.getElementById('bugs_display')
+const healthDisp = document.getElementById('health_display')
+const budgetDisp = document.getElementById('budget_display')
+const inputs = ['features_input', 'bugs_input', 'refactor_input']
+const sliders = inputs.map(id => document.getElementById(id))
+const values = inputs.map(id => id.replace('input', 'value'))
+// let percents = inputs.map(id => id.replace("input", "percent"));
 // Initial situation
-let mySys = new SimSys();
-
-
+const mySys = new SimSys()
 
 // Check the cookie
-if (document.cookie.indexOf("state") !== -1) {
-    const cookies = document.cookie.split(";");
-    for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookies[i];
-        const parts = cookie.split("=");
-        if (parts[0] === "state") {
-            mySys.fromCookie(parts[1]);
-        }
+if (document.cookie.indexOf('state') !== -1) {
+  const cookies = document.cookie.split(';')
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i]
+    const parts = cookie.split('=')
+    if (parts[0] === 'state') {
+      mySys.fromCookie(parts[1])
     }
+  }
 }
 
 // Allocate the budget in the three different inputs
-let prevInputs = [];
-sliders.forEach(input => input.addEventListener("input", mySys.updateSum));
+const prevInputs = []
+sliders.forEach(input => input.addEventListener('input', mySys.updateSum))
 
-iterate_btn.addEventListener("click", function () {
-    mySys.runIteration(2);
-    document.cookie = mySys.toString();
+iterateBtn.addEventListener('click', function () {
+  mySys.runIteration(2)
+  document.cookie = mySys.toString()
 })
 
-reset_btn.addEventListener("click", function () {
-    mySys.resetSimulation();
-    sliders.forEach(slider => slider.value = 0);
-    //percents.forEach((id, i) => document.getElementById(id).textContent = sliders[i].value);
-    values.forEach((id, i) => document.getElementById(id).textContent = sliders[i].value);
-    document.cookie = mySys.toString();
+resetBtn.addEventListener('click', function () {
+  mySys.resetSimulation()
+  sliders.forEach(slider => (slider.value = 0))
+  // percents.forEach((id, i) => document.getElementById(id).textContent = sliders[i].value);
+  values.forEach((id, i) => (document.getElementById(id).textContent = sliders[i].value))
+  document.cookie = mySys.toString()
 })
 
-download_btn.addEventListener("click", function () {
-    fetch('/download', {
-        method: "POST",
-        body: mySys.toString(),
-        headers: { "Content-Type": "application/json" }
+downloadBtn.addEventListener('click', function () {
+  fetch('/download', {
+    method: 'POST',
+    body: mySys.toString(),
+    headers: { 'Content-Type': 'application/json' }
+  })
+})
+
+uploadBtn.addEventListener('click', function () {
+  fetch('/state.json', {
+    method: 'GET'
+  })
+    .then(response => response.json())
+    .then(data => {
+      mySys.iteration = data.iteration
+      mySys.config = data.config
+      mySys.stats = data.stats
+      mySys.budget = data.budget
+      mySys.updateDisplays()
     })
-})
-
-upload_btn.addEventListener("click", function () {
-    fetch('/state.json', {
-        method: "GET"
+    .catch(error => {
+      console.log('ERROR: ' + error)
     })
-        .then(response => response.json())
-        .then(data => {
-            mySys.iteration = data.iteration;
-            mySys.config = data.config;
-            mySys.stats = data.stats;
-            mySys.budget = data.budget;
-            mySys.updateDisplays();
-        })
-        .catch(error => {
-            console.log("ERROR: " + error);
-        })
 })
--- v2/server2.js
+++ v2/server2.js
@@ -1,9 +1,9 @@
-const http = require('http');
-const fs = require('fs');
+const http = require('http')
+const fs = require('fs')
 
-const PUERTO = 9090;
+const PUERTO = 9090
 
-const pagina_error = `
+const errorPage = `
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -19,80 +19,74 @@ const pagina_error = `
 </html>
 `
 
-
-//Inicialización de variables
-let fichero = '';
+// Inicialización de variables
+let fichero = ''
 
 const server = http.createServer((req, res) => {
+  console.log('_____________________________________________')
+  console.log('Petición recibida!')
 
-    console.log("_____________________________________________");
-    console.log("Petición recibida!");
+  // Valores por defecto de la respuesta
+  let code = 200
+  let codeMsg = 'OK'
+  let page = ''
 
-    //Valores por defecto de la respuesta
-    let code = 200;
-    let code_msg = 'OK';
-    let page = '';
-    
-    //Construyo la URL que ha solicitado el cliente y extraigo el recurso solicitado
-    const url = new URL(req.url, 'http://' + req.headers['host']);
-    console.log('RECURSO PEDIDO: ' + url.pathname);
+  // Construyo la URL que ha solicitado el cliente y extraigo el recurso solicitado
+  const url = new URL(req.url, 'http://' + req.headers.host)
+  console.log('RECURSO PEDIDO: ' + url.pathname)
 
-
-    // Saco el nombre del fichero que tengo que buscar
-    if (url.pathname=='/') {
-        fichero = 'index.html';
-    } else if (url.pathname=='/download') {
-        let stateData = '';
-        req.on('data', chunk => {
-            stateData += chunk;
-        })
-        req.on('end', () => {
-            fs.writeFileSync("state.json", stateData)
-        })
-    }else {
-        fichero = url.pathname.slice(1);
-    }
-    console.log("FICHERO QUE SE BUSCA: " + fichero);
-
-
-
-    //Lectura asincrona del fichero
-    fs.readFile(fichero, (err, data) => {
-        
-        if (err) {
-            //Si no se encuentra el fichero
-            console.log("Error!");
-            console.log(err.message);
-            code = 404;
-            code_msg = "Not Found";
-            res.setHeader('Content-Type', 'text/html');
-            page = pagina_error;
-        } else {
-            console.log("Fichero encontrado!");
-            //Extraigo la extension del nombre del fichero segun cual sea
-            //hago la respuesta que corresponda
-            punto = fichero.indexOf('.');
-            extension = fichero.slice(punto + 1);
-            if (extension == 'html'){
-                res.setHeader('Content-Type', 'text/html');
-            } else if (extension == 'js') {
-                res.setHeader('Content-Type', 'application/javascript');
-            } else if (extension == 'css') {
-                res.setHeader('Content-Type', 'text/css');
-            } else if (extension == 'json') {
-                res.setHeader('Content-Type', 'application/json');
-            }
-            page = data;
-        }
-        //Asigno los valores de la respuesta y la envio
-        res.statusCode = code;
-        res.statusMessage = code_msg;
-        res.write(page);
-        res.end();
-        console.log("Respuesta enviada!");
+  // Saco el nombre del fichero que tengo que buscar
+  if (url.pathname === '/') {
+    fichero = 'index.html'
+  } else if (url.pathname === '/download') {
+    let stateData = ''
+    req.on('data', chunk => {
+      stateData += chunk
     })
-});
+    req.on('end', () => {
+      fs.writeFileSync('state.json', stateData)
+    })
+  } else {
+    fichero = url.pathname.slice(1)
+  }
+  console.log('FICHERO QUE SE BUSCA: ' + fichero)
 
-//Lanzo el servidor
-server.listen(9090);
-console.log('Servidor escuchando en el puerto ' + PUERTO);
+  // Lectura asincrona del fichero
+  fs.readFile(fichero, (err, data) => {
+    if (err) {
+      // Si no se encuentra el fichero
+      console.log('Error!')
+      console.log(err.message)
+      code = 404
+      codeMsg = 'Not Found'
+      res.setHeader('Content-Type', 'text/html')
+      page = errorPage
+    } else {
+      console.log('Fichero encontrado!')
+      // Extraigo la extension del nombre del fichero segun cual sea
+      // hago la respuesta que corresponda
+      const punto = fichero.indexOf('.')
+      const extension = fichero.slice(punto + 1)
+      if (extension === 'html') {
+        res.setHeader('Content-Type', 'text/html')
+      } else if (extension === 'js') {
+        res.setHeader('Content-Type', 'application/javascript')
+      } else if (extension === 'css') {
+        res.setHeader('Content-Type', 'text/css')
+      } else if (extension === 'json') {
+        res.setHeader('Content-Type', 'application/json')
+      }
+      page = data
+    }
+    // Asigno los valores de la respuesta y la envio
+    res.statusCode = code
+    res.statusMessage = codeMsg
+    res.write(page)
+    res.end()
+    console.log('Respuesta enviada!')
+  })
+})
+
+// Lanzo el servidor
+server.listen(9090)
+console.log('Servidor escuchando en el puerto ' + PUERTO)
